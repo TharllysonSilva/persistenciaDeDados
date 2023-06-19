116bcbec98d7f2257f1e35bca8bbfb58337aa996
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-06-19 17:18:29 -0300

    criando metodos de salvar. pesquisar  e deletar

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,3 +1,5 @@
+import 'package:nosso_primeiro_projeto/components/task.dart';
+
 class TaskDao {
   static const String tableSql =
       'CREATE TABLE $_tablename($_name TEXT, $_difficulty INTEGER, $_image TEXT,)';
@@ -6,4 +8,9 @@ class TaskDao {
   static const String _name = 'name';
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
+
+  save(Task tarefa) async {}
+  Future<List<Task>> findAll() async {}
+  Future<List<Task>> find(String nomeDaTarefa) async {}
+  delete(String nomeDaTarefa) async {}
 }
